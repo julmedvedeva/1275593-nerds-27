cbaf895370b41bae4b8361bc6497a19e3c47dbd4
Julia <medvedevajulev@gmail.com>
2020-03-21 23:47:34 +0300

    first raund

--- js/script.js
+++
@@ -1,8 +0,0 @@
-var link = document.querySelector('.button-feedback');
-var popup = document.querySelector('.form-feedback-page');
-var close = popup.querySelector('.form-feedback-closed');
-
-link.addEventListener('click', function(event) {
-	event.preventDefault()
-	popup.classList.add('.show');
-})
